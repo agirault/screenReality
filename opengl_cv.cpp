2654b8e2dbbe1443ad0f5dc41ffbe762e310525d
agirault <girault.al@gmail.com>
2014-12-09 22:14:07 -0500

    UPD : add 3d scene moving

--- opengl_cv.cpp
+++ opengl_cv.cpp
@@ -12,12 +12,16 @@
 
 /** Constants */
 const float camRatio = 0.5;
-const float sceneRatio = 1.5;
+const float windowRatio = 1.0;
 const bool bDisplayDetection = true;
+const bool bFullScreen = true;
 
-const int minFaceSize = 100; // in pixel. The smaller it is, the further away you can go
+const int minFaceSize = 80; // in pixel. The smaller it is, the further away you can go
 const float cvCamViewAngleXDeg = 48.55;
 const float cvCamViewAngleYDeg = 40.37;
+const float cx = 362.9;
+const float cy = 395.84;
+const float f = 804.71;
 const float eyesGap = 6.5; //cm
 
 
@@ -28,6 +32,11 @@ cv::CascadeClassifier face_cascade;
 cv::VideoCapture *capture = NULL;
 cv::Mat frame;
 
+int windowWidth;
+int windowHeight;
+int camWidth;
+int camHeight;
+
 float glCamX;
 float glCamY;
 float glCamZ;
@@ -36,11 +45,16 @@ float glCamZ;
 void redisplay();
 cv::Mat detectEyes(cv::Mat image);
 
+void setGlCamera();
+void draw3dScene();
+void drawCube();
+void drawAxes(float length);
+
 void onReshape( int w, int h );
 void onMouse( int button, int state, int x, int y );
 void onKeyboard( unsigned char key, int x, int y );
 void onIdle();
-void drawAxes(float length);
+
 
 /**
  * @function main
@@ -51,13 +65,6 @@ int main( int argc, char **argv )
     //-- Load the cascades
     if( !face_cascade.load( face_cascade_name ) )
     { printf("-- (!) ERROR loading 'haarcascade_frontalface_alt.xml'\nPlease edit face_cascade_name in source code.\n"); return -1; };
-    if( !eyes_cascade.load( eyes_cascade_name ) )
-    { printf("-- (!) ERROR loading 'haarcascade_eye_tree_eyeglasses.xml'\nPlease edit eyes_cascade_name in source code.\n"); return -1; };
-
-    //-- Create Windows
-    cv::namedWindow(window_face_detection);
-    cv::moveWindow(window_face_detection, 400, 100);
-
 
     // VIDEO CAPTURE
     //-- start video capture from camera
@@ -69,16 +76,38 @@ int main( int argc, char **argv )
         return 1;
     }
 
-    // WINDOW DIMENSIONS
-    int width = (int) capture->get( CV_CAP_PROP_FRAME_WIDTH );
-    int height = (int) capture->get( CV_CAP_PROP_FRAME_HEIGHT );
+    // CAMERA IMAGE DIMENSIONS
+    camWidth = (int) capture->get( CV_CAP_PROP_FRAME_WIDTH );
+    camHeight = (int) capture->get( CV_CAP_PROP_FRAME_HEIGHT );
 
     // GLUT INIT
     glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
-    glutInitWindowPosition( 20, 20 );
-    glutInitWindowSize( width*sceneRatio, height*sceneRatio );
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    if(!bFullScreen){
+        windowWidth = camWidth*windowRatio;
+        windowHeight = camHeight*windowRatio;
+        glutInitWindowPosition( 20, 20 );
+        glutInitWindowSize( windowWidth, windowHeight );
+    }
     glutCreateWindow( "Test OpenGL" );
+
+    // RENDERING INIT
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING); //Enable lighting
+    glEnable(GL_LIGHT0); //Enable light #0
+    glEnable(GL_LIGHT1); //Enable light #1
+    glEnable(GL_NORMALIZE); //Automatically normalize normals
+    glShadeModel(GL_SMOOTH); //Enable smooth shading
+
+    // SCREEN DIMENSIONS
+    windowWidth = glutGet(GLUT_SCREEN_WIDTH);
+    windowHeight = glutGet(GLUT_SCREEN_HEIGHT);
+    if(bFullScreen)
+    {
+        glutFullScreen();
+        glViewport( 0, 0, windowWidth, windowHeight );
+    }
 
     // GUI CALLBACK FUNCTIONS
     glutDisplayFunc( redisplay );
@@ -99,38 +128,22 @@ int main( int argc, char **argv )
 void redisplay()
 {
     if(frame.empty()) {return;}
-    glClear( GL_COLOR_BUFFER_BIT );
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /** OPENCV */
+    // OPENCV
     //-- flip frame image
     cv::Mat tempimage;
     cv::flip(frame, tempimage, 0);
     //-- detect eyes
     tempimage = detectEyes(tempimage);
 
-
-    /** OPENGL */
-    // CAMERA PARAMETERS
-    //-- set projection matrix using intrinsic camera params
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    //-- gluPerspective is arbitrarily set, you will have to determine these values based
-    gluPerspective(60, tempimage.size().width*1.0/tempimage.size().height, 1, 20);
-    //-- you will have to set modelview matrix using extrinsic camera params
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-
-    // 3D SCENE
-    glPushMatrix();
-    drawAxes(1.0);
-    glutSolidTeapot(1.0);
-    glTranslatef(0.0f, 0.0f, 2.0f);
-    glPopMatrix();
+    // OPENGL
+    setGlCamera();
+    draw3dScene();
 
     //-- display camera frame
     cv::flip(tempimage, tempimage, 0);
-    cv::resize(tempimage, tempimage, cv::Size(camRatio*tempimage.size().width, camRatio*tempimage.size().height), 0, 0, cv::INTER_CUBIC);
+    cv::resize(tempimage, tempimage, cv::Size(camRatio*camWidth, camRatio*camHeight), 0, 0, cv::INTER_CUBIC);
     glDrawPixels( tempimage.size().width, tempimage.size().height, GL_BGR, GL_UNSIGNED_BYTE, tempimage.ptr() );
 
     // RENDER
@@ -159,34 +172,44 @@ cv::Mat detectEyes(cv::Mat image) {
         cv::Point rightEyePt( faces[i].x + faces[i].width*0.70, faces[i].y + faces[i].height*0.37 );
         cv::Point eyeCenterPt( faces[i].x + faces[i].width*0.5, leftEyePt.y );
 
+        float testX1 = (rightEyePt.x - cx)/f;
+        float testX2 = (leftEyePt.x - cx)/f;
+        float spaceZ = 0.5*eyesGap/(testX1-testX2);
+        std::cout<<spaceZ<<std::endl;
+
         // COORDINATES
         //-- z
         int imgEyesGap = rightEyePt.x-leftEyePt.x;
-        glCamZ = (550.0*eyesGap)/imgEyesGap; // doesn't look right
+        float tempsGLCamZ = (550.0*eyesGap)/imgEyesGap;
+        glCamZ = (tempsGLCamZ+glCamZ)/2.0;
         //-- x
-        float ratioX = (float)eyeCenterPt.x/image.size().width;
+        float ratioX = (float)eyeCenterPt.x/camWidth;
         if(ratioX <= 0.5){
             float phi1 = cvCamViewAngleXDeg * ratioX;
             float phi2 = (cvCamViewAngleXDeg/2.0)-phi1;
-            glCamX = -glCamZ*tan(phi2*M_PI/180.0);
+            float tempGLCamX = -glCamZ*tan(phi2*M_PI/180.0);
+            glCamX = (tempGLCamX+glCamX)/2.0;
         }else if(ratioX > 0.5){
             float phi1 = cvCamViewAngleXDeg * ratioX;
             float phi2 = phi1-(cvCamViewAngleXDeg/2.0);
-            glCamX = glCamZ*tan(phi2*M_PI/180.0);
+            float tempGLCamX = glCamZ*tan(phi2*M_PI/180.0);
+            glCamX = (tempGLCamX+glCamX)/2.0;
         }
         //-- y
-        float ratioY = (float)eyeCenterPt.y/image.size().height;
+        float ratioY = (float)eyeCenterPt.y/camHeight;
         if(ratioY <= 0.5){
             float phi1 = cvCamViewAngleYDeg * ratioY;
             float phi2 = (cvCamViewAngleYDeg/2.0)-phi1;
-            glCamY = glCamZ*tan(phi2*M_PI/180.0);
+            float tempGLCamY = glCamZ*tan(phi2*M_PI/180.0);
+            glCamY = (tempGLCamY+glCamY)/2.0;
         }else if(ratioY > 0.5){
             float phi1 = cvCamViewAngleYDeg * ratioY;
             float phi2 = phi1-(cvCamViewAngleYDeg/2.0);
-            glCamY = -glCamZ*tan(phi2*M_PI/180.0);
+            float tempGLCamY = -glCamZ*tan(phi2*M_PI/180.0);
+            glCamY = (tempGLCamY+glCamY)/2.0;
         }
         //-- cout
-        std::cout<<"(x,y,z) = ("<<glCamX<<","<<glCamY<<","<<glCamZ<<")"<<std::endl;
+        //std::cout<<"(x,y,z) = ("<<(int)glCamX<<","<<(int)glCamY<<","<<(int)glCamZ<<")"<<std::endl;
 
         //DISPLAY
         if(bDisplayDetection)
@@ -213,6 +236,139 @@ cv::Mat detectEyes(cv::Mat image) {
     }
 
     return image;
+}
+
+void setGlCamera()
+{
+    // CAMERA PARAMETERS
+    //-- set projection matrix using intrinsic camera params
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //-- gluPerspective is arbitrarily set, you will have to determine these values based
+    gluPerspective(60, windowWidth*1.0/windowHeight, 1, 20);
+    //-- you will have to set modelview matrix using extrinsic camera params
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(glCamX/10.0, glCamY/10.0, glCamZ/10.0, 0, 0, 0, 0, 1, 0);
+}
+
+void draw3dScene()
+{
+    // LIGHTING
+    //-- Add ambient light
+    GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+
+    //-- Add positioned light
+    GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
+    GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+
+    //-- Add directed light
+    GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
+    //Coming from the direction (-1, 0.5, 0.5)
+    GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+    glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
+
+    // GEOMETRY
+    //-- Pose and Color
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 1.0f, 0.0f);
+    drawCube();
+
+    //-- Pose and Color
+    glTranslatef(-2.0f, 0.0f, -2.0f);
+    glRotatef(70.0f, 0.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 1.0f);
+    drawCube();
+
+    //-- Pose and Color
+    glTranslatef(0.0f, 0.0f, 6.0f);
+    glRotatef(10.0f, 0.0f, 1.0f, 0.0f);
+    glColor3f(0.0f, 1.0f, 1.0f);
+    drawCube();
+
+
+}
+
+void drawCube()
+{
+    glBegin(GL_QUADS);
+    //Front
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    //glNormal3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    //glNormal3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    //glNormal3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    //glNormal3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    //Right
+    glNormal3f(1.0f, 0.0f, 0.0f);
+    //glNormal3f(1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    //glNormal3f(1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    //glNormal3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    //glNormal3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+
+    //Back
+    glNormal3f(0.0f, 0.0f, -1.0f);
+    //glNormal3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    //glNormal3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    //glNormal3f(1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    //glNormal3f(1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+
+    //Left
+    glNormal3f(-1.0f, 0.0f, 0.0f);
+    //glNormal3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    //glNormal3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    //glNormal3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    //glNormal3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+
+    glEnd();
+}
+
+/**
+ * @function drawAxes
+ * A useful function for displaying your coordinate system
+ */
+void drawAxes(float length)
+{
+  glPushAttrib(GL_POLYGON_BIT | GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT) ;
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) ;
+
+  glBegin(GL_LINES) ;
+  glColor3f(1,0,0) ;
+  glVertex3f(0,0,0) ;
+  glVertex3f(length,0,0);
+
+  glColor3f(0,1,0) ;
+  glVertex3f(0,0,0) ;
+  glVertex3f(0,length,0);
+
+  glColor3f(0,0,1) ;
+  glVertex3f(0,0,0) ;
+  glVertex3f(0,0,length);
+  glEnd() ;
+
+  glPopMatrix() ;
 }
 
 /**
@@ -259,30 +415,3 @@ void onIdle()
     (*capture) >> frame;
 }
 
-/**
- * @function drawAxes
- * A useful function for displaying your coordinate system
- */
-void drawAxes(float length)
-{
-  glPushAttrib(GL_POLYGON_BIT | GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT) ;
-
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) ;
-  glDisable(GL_LIGHTING) ;
-
-  glBegin(GL_LINES) ;
-  glColor3f(1,0,0) ;
-  glVertex3f(0,0,0) ;
-  glVertex3f(length,0,0);
-
-  glColor3f(0,1,0) ;
-  glVertex3f(0,0,0) ;
-  glVertex3f(0,length,0);
-
-  glColor3f(0,0,1) ;
-  glVertex3f(0,0,0) ;
-  glVertex3f(0,0,length);
-  glEnd() ;
-
-  glPopAttrib() ;
-}
